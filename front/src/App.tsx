d1a13e4bc511d0fa634a1ab5c90f9726d3b05ddc
Ilyass Baba <ilyassbaba.me@gmail.com>
2025-01-02 17:15:46 +0100

    SYNC

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/users" element={<Dashboard />} />
         {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
